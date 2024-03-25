30ad8a7005b3c9930ee3d7b535a348cff4829e34
Mihai <cataraga.95@gmail.com>
2024-03-26 01:34:51 +0200

    new

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,36 +1,39 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom'
+import React, { useEffect } from 'react';
+import { Link, NavLink } from 'react-router-dom';
+import Stema from '../assets/img/stemaRM.png'
 
 export default function Navbar() {
-  return (
-    <div className='navbar'>
-        <div className='cube'>
-            <div className='row'>
-                <div className='square'></div>
-                <div className='square'></div>
-                <div className='square'></div>
-            </div>
-            <div className='row'>
-                <div className='square'></div>
-                <div className='square'></div>
-                <div className='square'></div>
-            </div>
-            <div className='row'>
-                <div className='square'></div>
-                <div className='square'></div>
-                <div className='square'></div>
-            </div>
+    useEffect(() => {
+        const navbarScroll = () => {
+            const scrollY = window.scrollY || window.pageYOffset;
+            const elem = document.getElementById('navbar')
+            if (scrollY >= 80) {
+                elem.style.background = 'rgb(0, 46, 28)';
+                elem.style.transition = '0.5s ease-in-out';
+            } else{
+                elem.style.background = 'transparent';
+
+            }
+        }
+        // Adaugă event listener pentru scroll
+        window.addEventListener('scroll', navbarScroll);
+        // Cleanup: elimină event listener-ul când componenta este dezmontată
+        return () => {
+            window.removeEventListener('scroll', navbarScroll);
+        };
+    }, [])
+    return (
+        <div className='navbar' id='navbar'>
+            <Link className='logo'>
+                <img src={Stema} alt='Logo img' />
+                <span>RM</span>
+            </Link>
+            <nav>
+                <NavLink to={'/'}>Home</NavLink>
+                <NavLink to={'/'}>Home</NavLink>
+                <NavLink to={'/'}>Home</NavLink>
+                <NavLink to={'/'}>Home</NavLink>
+            </nav>
         </div>
-        <div className='logo'>
-            <img src='' alt='Logo img' />
-            <span>RM</span>
-        </div>
-        <nav>
-            <NavLink to={'/'}>Home</NavLink>
-            <NavLink to={'/'}>Home</NavLink>
-            <NavLink to={'/'}>Home</NavLink>
-            <NavLink to={'/'}>Home</NavLink>
-        </nav>
-    </div>
-  )
+    )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,9 +3,17 @@ import Navbar from '../components/Navbar'
 
 export default function Home() {
   return (
-    <div>
+    <div className='home'>
         <Navbar />
-        home
+        <div className='sec-1'>
+          <div className='hexagon1'></div>
+          <div className='hexagon2'></div>
+          <div className='hexagon3'></div>
+          <div className='hexagon4'></div>
+          <div className='hexagon5'></div>
+          <div className='hexagon6'></div>
+          <div className='hexagon7'></div>
+        </div>
     </div>
   )
 }
