915b891ffde5a93e92a6ffde453643916760dcb9
Mihai Cataraga <mihaicataraga@MacBook-Pro-Mihai.local>
2024-03-26 04:40:22 +0200

    new2

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,13 +6,18 @@ export default function Home() {
     <div className='home'>
         <Navbar />
         <div className='sec-1'>
-          <div className='hexagon1'></div>
-          <div className='hexagon2'></div>
-          <div className='hexagon3'></div>
-          <div className='hexagon4'></div>
-          <div className='hexagon5'></div>
-          <div className='hexagon6'></div>
-          <div className='hexagon7'></div>
+          <div className='card'>
+            <h3>Republic of</h3>
+            <h1>Moldova</h1>
+            <p>Moldova, a country located in Eastern Europe, known for its varied landscapes, which include mountains, forests and picturesque rivers, as well as its rich and diverse cultural heritage.</p>
+          </div>
+          <div className='hexagon1 hex'></div>
+          <div className='hexagon2 hex'></div>
+          <div className='hexagon3 hex'></div>
+          <div className='hexagon4 hex'></div>
+          <div className='hexagon5 hex'></div>
+          <div className='hexagon6 hex'></div>
+          <div className='hexagon7 hex'></div>
         </div>
     </div>
   )
